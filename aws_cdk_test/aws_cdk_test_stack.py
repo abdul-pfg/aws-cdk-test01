3972266b94f06176b7389e561099e601c31d070f
abdul-pfg <66666645+abdul-pfg@users.noreply.github.com>
2020-06-28 22:41:54 +0530

    Add files via upload

---
+++ aws_cdk_test/aws_cdk_test_stack.py
@@ -0,0 +1,14 @@
+from aws_cdk import core
+import aws_cdk.aws_s3 as s3
+
+
+class AwsCdkTestStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        bucket = s3.Bucket(self,
+    		"BucketCDK", 
+    		versioned=True,
+    		bucket_name='cdk-managed-bucket-s843971')
+        
